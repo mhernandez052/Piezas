343f59acb152b0d7fd95149741d474833f3fe0cd
mhernandez052 <mjhernandez@mail.csuchico.edu>
2019-12-10 22:13:59 -0800

    Failing Test Adjusting

--- Piezas.cpp
+++ Piezas.cpp
@@ -129,7 +129,7 @@ Piece Piezas::gameState() {
   for (int i = 0; i < BOARD_COLS; i++) {
     int curX = 1;
     int curO = 1;
-    for (int j = 0; j < BOARD_ROWS - 1; j++) {
+    for (int j = 0; j < BOARD_ROWS; j++) {
       if (curO > MaxO) {
         MaxO = curO;
       }
